102cc094ec229761ee088fe814967d188c7b40d6
kirstenalarsen <kirsten@eaterprises.com.au>
2013-01-09 22:56:14 +1100

    changes to reports

--- lib/open_food_web/order_and_distributor_report.rb
+++ lib/open_food_web/order_and_distributor_report.rb
@@ -8,8 +8,8 @@ module OpenFoodWeb
 
     def header
       ["Order date", "Order Id",
-          "Customer Name","Customer Email", "Customer Phone", "Customer City",
-          "SKU", "Item name", "Variant", "Quantity", "Max Quantity", "Cost", "Shipping cost",
+          "Customer Name","Customer Email", "Customer Phone", "Customer Address","Customer City",
+          "Supplier", "Item name", "Variant", "Quantity", "Max Quantity", "Cost", "Shipping cost",
           "Payment method",
           "Distributor", "Distributor address", "Distributor city", "Distributor postcode", "Shipping instructions"]
     end
@@ -19,8 +19,8 @@ module OpenFoodWeb
       @orders.each do |order|
         order.line_items.each do |line_item|
           order_and_distributor_details << [order.created_at, order.id,
-            order.bill_address.full_name, order.email, order.bill_address.phone, order.bill_address.city,
-            line_item.product.sku, line_item.product.name, line_item.variant.options_text, line_item.quantity, line_item.max_quantity, line_item.price * line_item.quantity, line_item.itemwise_shipping_cost,
+            order.bill_address.full_name, order.email, order.bill_address.phone, order.bill_address.address1, order.bill_address.city,
+            line_item.product.supplier.name, line_item.product.name, line_item.variant.options_text, line_item.quantity, line_item.max_quantity, line_item.price * line_item.quantity, line_item.itemwise_shipping_cost,
             order.payments.first.payment_method.andand.name,
             order.distributor.andand.name, order.distributor.address.address1, order.distributor.address.city, order.distributor.address.zipcode, order.special_instructions ]
         end
