91e703edc8656f768f74a2f8c667b33fa10e1b6c
kirstenalarsen <kirsten@eaterprises.com.au>
2013-01-24 22:21:36 +1100

    Fix merge clash

--- lib/open_food_web/order_and_distributor_report.rb
+++ lib/open_food_web/order_and_distributor_report.rb
@@ -18,15 +18,9 @@ module OpenFoodWeb
       order_and_distributor_details = []
       @orders.each do |order|
         order.line_items.each do |line_item|
-<<<<<<< HEAD
-          order_and_distributor_details << [order.completed_at, order.id,
-            order.bill_address.full_name, order.email, order.bill_address.phone, order.bill_address.city,
-            line_item.product.sku, line_item.product.name, line_item.variant.options_text, line_item.quantity, line_item.max_quantity, line_item.price * line_item.quantity, line_item.itemwise_shipping_cost,
-=======
           order_and_distributor_details << [order.created_at, order.id,
             order.bill_address.full_name, order.email, order.bill_address.phone, order.bill_address.address1, order.bill_address.city,
             line_item.product.supplier.name, line_item.product.name, line_item.variant.options_text, line_item.quantity, line_item.max_quantity, line_item.price * line_item.quantity, line_item.itemwise_shipping_cost,
->>>>>>> 102cc09... changes to reports
             order.payments.first.payment_method.andand.name,
             order.distributor.andand.name, order.distributor.address.address1, order.distributor.address.city, order.distributor.address.zipcode, order.special_instructions ]
         end
